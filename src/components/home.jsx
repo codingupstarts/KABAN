fd3f120e22e3167d596bee8e519db2ce5699de86
codingupstarts <codingmythz@gmail.com>
2021-08-27 11:23:40 +0100

    add App bar

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./home/nav/Navbar";
+
+
+
+const Home = () => {
+    return (
+       <>
+       <Navbar/>
+       </>
+    )
+}
+
+
+export default Home;
